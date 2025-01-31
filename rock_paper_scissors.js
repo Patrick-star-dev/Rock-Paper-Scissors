927149c5af428aa0c5103054346f96eb02f6f9f8
Patrick-star-dev <194951713+Patrick-star-dev@users.noreply.github.com>
2025-01-31 15:52:37 +0330

    adding playround function, every round this function runs and checks who won

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -2,4 +2,40 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let humanChoice = prompt("Enter your choice (rock, paper, scissors):");
+    if (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
+        return "Your choice wasn't valid!";
+    } else {
+        return humanChoice;
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = String(humanChoice).toLowerCase(); // making human choice case isensetive
+    if (humanChoice === computerChoice) {
+        return "draw";
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        computerScore++;
+        return "You lose! paper beats rock";
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore++;
+        return "You win! rock beats scissors";
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        computerScore++;
+        return "You lose! scissors beats paper";
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore++;
+        return "You win! paper beats rock!";
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore++;
+        return "You lose! rock beats scissors";
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++;
+        return "You win! scissors beats paper";
+    }
+}
+
